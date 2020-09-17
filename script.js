8bdde1003c7704f3a6400e7fe4dbcaf09d459eea
sakilk130 <sakilk130@gmail.com>
2020-09-18 03:30:44 +0600

    add loader

--- script.js
+++ script.js
@@ -3,13 +3,27 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+function compete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 //Get Quote from API
 async function getQuote() {
+  loading();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl =
     'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
+    // const response = await fetch(proxyUrl + apiUrl);
     const response = await fetch(apiUrl);
     const data = await response.json();
 
@@ -24,6 +38,8 @@ async function getQuote() {
       quoteText.classList.remove('long-quote');
     }
     quoteText.innerText = data.quoteText;
+
+    compete();
   } catch (error) {
     getQuote();
   }
